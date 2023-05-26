1d9d82204f38c639a89655d0204e23e8c55c7261
crazywoola <li.zheng@dentsplysirona.com>
2023-05-26 14:40:37 +0800

    poc: test ruby sinatra streaming

---
+++ app/server.rb
@@ -0,0 +1,15 @@
+require 'sinatra'
+
+get '/' do
+  'Hello world!'
+end
+
+get '/stream' do
+    stream do |out|
+        out << "It's gonna be legen -\n"
+        sleep 0.5
+        out << " (wait for it) \n"
+        sleep 1
+        out << "- dary!\n"
+    end
+end
